b3b5bee27cebbef4ef4828fa2c672f60143cf033
Mirko Martinovic <bgmirko@hotmail.com>
2020-02-02 22:24:10 +0100

    Small code cleaning

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 const Performer = require('../models/performer');
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -3,19 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
   },
-  email: {
-    type: String,
-    required: true
-  },
   userName: {
-    type: String,
-    required: true
-  },
-  email: {
     type: String,
     required: true
   },
